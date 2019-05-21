6f0f430d671442b2d0efbf7fd7c5583fff63d40d
MinChangs <bijou0147@gmail.com>
2019-05-21 12:27:53 +0900

    jsp git으로 올리가

---
+++ src/main/java/kr/or/ddit/servlet/BasicGugudan.java
@@ -0,0 +1,45 @@
+package kr.or.ddit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BasicGugudan extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		// localhost/jsp//timeServlet?param=6
+		String param = req.getParameter("i");
+		String param2 = req.getParameter("j");
+		System.out.println("param : " +param);
+		System.out.println("param2 : " +param2);
+		
+		pw.write("<html>");
+		pw.write("	<head>");
+		pw.write("		<title> Hello, World  </title>");
+		pw.write("	</head>");
+		pw.write("	<body>");
+		pw.write("	<table border='1'>");
+		for(int i=1; i<=Integer.parseInt(param2); i++){
+			pw.write("	<tr>");
+			for(int j=2; j<=Integer.parseInt(param); j++ ){
+				pw.write("	<td>"+j+" * "+i+" = "+i*j+"</td>");
+				
+			}
+			pw.write("	</tr>");
+			
+		}
+		pw.write("	</table>");
+		
+		pw.write("	</body>");
+		pw.write("</html>");
+		pw.close();
+	}
+
+}
---
+++ src/main/java/kr/or/ddit/servlet/BasicTest.java
@@ -0,0 +1,30 @@
+package kr.or.ddit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BasicTest extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		
+		SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+				
+		Calendar time = Calendar.getInstance();
+		       
+		String format_time1 = format1.format(time.getTime());
+		PrintWriter pw =resp.getWriter();
+		pw.write(format_time1);
+		pw.close();
+	}
+
+}
