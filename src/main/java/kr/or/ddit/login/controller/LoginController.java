8e114edfb9b867ad46b6ced23587223ede10ff1a
MinChangs <bijou0147@gmail.com>
2019-06-03 17:16:57 +0900

    암호화 알고리즘 적용  - Hash 알고리즘 로그인 요청 로직 적용

--- src/main/java/kr/or/ddit/login/controller/LoginController.java
+++ src/main/java/kr/or/ddit/login/controller/LoginController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import kr.or.ddit.encrypt.kisa.sha256.KISA_SHA256;
 import kr.or.ddit.user.model.UserVo;
 import kr.or.ddit.user.service.IUserService;
 import kr.or.ddit.user.service.UserService;
@@ -98,6 +99,7 @@ public class LoginController extends HttpServlet {
 		//사용자 파라미터 userId, password
 		String userId= request.getParameter("userId");
 		String password= request.getParameter("password");
+		String encryptPassword = KISA_SHA256.encrypt(password);
 		//db에서 해당 사용자의 정보 조회(service, dao)
 		
 		//해당 사용자 정보를 이용하여 사용자가 보낸 userId, password가 일치하는지 검사
@@ -108,7 +110,7 @@ public class LoginController extends HttpServlet {
 		String passcheck = vo.getPass();
 		
 		//일치하면 (로그인 성공) : main화면으로 이동
-		if(vo!=null&&userId.equals(idcheck)&&password.equals(passcheck)){
+		if(vo!=null&&userId.equals(idcheck)&&encryptPassword.equals(passcheck)){
 			
 			//remember 파라미터가 존재할 경우 userId, rememberme cookie설정해준다
 			//remember 파라미터가 존재하지 않을 경우 userId, rememberme cookie 삭제한다
